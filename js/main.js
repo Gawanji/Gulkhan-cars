02afb06fb6637a96266b2c61792f83a781aa9fe7
gul <gul@gmail.com>
2025-12-12 11:25:18 +0500

    Initial commit with WhatsApp booking feature

---
+++ js/main.js
@@ -0,0 +1,140 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+
+
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+
+    $(window).on("load resize", function () {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+                function () {
+                    const $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "true");
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function () {
+                    const $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "false");
+                    $this.find($dropdownMenu).removeClass(showClass);
+                }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 25,
+        dots: true,
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+    // WhatsApp Booking Logic
+    $('#bookingForm').on('submit', function (e) {
+        e.preventDefault();
+
+        var name = $('#nameInput').val();
+        var email = $('#emailInput').val();
+        var service = $('#serviceSelect option:selected').text();
+        var date = $('#dateInput').val();
+        var request = $('#requestInput').val();
+
+        // Ensure values are safe to check
+        if (!name) name = "N/A";
+        if (!email) email = "N/A";
+        if (service === "Select A Service") service = "General Inquiry";
+        if (!date) date = "N/A";
+        if (!request) request = "None";
+
+        var message = "New Booking Request\n" +
+            "Name: " + name + "\n" +
+            "Email: " + email + "\n" +
+            "Service: " + service + "\n" +
+            "Date: " + date + "\n" +
+            "Special Request: " + request;
+
+        var encodedMessage = encodeURIComponent(message);
+        var url = "https://wa.me/77074715776?text=" + encodedMessage;
+
+        window.open(url, '_blank');
+    });
+
+})(jQuery);
+
